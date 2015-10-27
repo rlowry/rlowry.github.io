6688bca7333cc1051327fbd038e6302362715b8e
Ryan Lowry <r.lowry@rcsed.ac.uk>
2015-10-27 16:17:47 +0000

    Image Opt

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var imageop = require('gulp-image-optimization');
 
 gulp.task('default', function () {
   gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
@@ -7,4 +8,9 @@ gulp.task('default', function () {
     .pipe(gulp.dest('./js'));
   gulp.src('./bower_components/jquery/dist/jquery.min.js')
     .pipe(gulp.dest('./js'));
+  gulp.src(['img/**/*.png','img/**/*.jpg','img/**/*.gif','img/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('img'));
 });
